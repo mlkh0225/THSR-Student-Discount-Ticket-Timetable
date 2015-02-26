95e0847444712e0e2365873975ba01a71b1fb2cc
Chris Lai <mlkh0225@gmail.com>
2015-02-26 14:17:15 +0800

    v0.2.1 北上 Bug 修正

--- THSR.py
+++ THSR.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 baseDate = datetime(2015, 1, 4)
-userDate = raw_input("Date (Ex: 2015,02,25): ")
+userDate = raw_input("Ride date (Ex:2015,02,25): ")
 userDate = datetime.strptime(userDate, "%Y,%m,%d")
 weekday = int((userDate-baseDate).days)%7-1
 
@@ -39,19 +39,19 @@ t399 = [399,1332,-1,1351,-1,1384,1408,1426,1439,"50%"]
 t541 = [541,1350,1358,1372,1383,1410,-1,-1,-1,"50%"]
 t545 = [545,1380,1388,1402,1413,1439,-1,-1,-1,"50%"]
 
-t502 = [502,-1,-1,-1,390,417,429,443,450,"50%"]
-t604 = [604,396,411,431,457,484,496,509,516,"50%"]
-t1512 = [1512,-1,-1,-1,499,526,538,551,558,"50%"]
-t1618 = [1618,498,513,534,559,586,598,611,618,"50%"]
-t620 = [620,516,529,549,575,603,616,629,636,"50%"]
-t246 = [246,1194,-1,-1,1239,-1,-1,1282,1290,"50%"]
-t1750 = [1750,1218,1233,1254,1279,1306,1318,1331,1338,"50%"]
-t752 = [752,1236,1249,1269,1295,1323,1336,1349,1356,"50%"]
-t254 = [254,1254,-1,-1,1299,-1,-1,1342,1350,"50%"]
-t756 = [756,1260,1275,1296,1321,1348,1360,1373,1380,"50%"]
-t762 = [762,1314,1329,1350,1375,1402,1414,1427,1434,"50%"]
-t334 = [334,1332,1345,1365,1390,-1,-1,1433,1439,"50%"]
-t596 = [596,1380,1395,1415,1439,-1,-1,-1,-1,"50%"]
+t502 = [502,450,443,429,417,390,-1,-1,-1,"50%"]
+t604 = [604,516,509,496,484,457,431,411,396,"50%"]
+t1512 = [1512,558,551,538,526,499,-1,-1,-1,"50%"]
+t1618 = [1618,618,611,598,586,559,534,513,498,"50%"]
+t620 = [620,636,629,616,603,575,549,529,516,"50%"]
+t246 = [246,1290,1282,-1,-1,1239,-1,-1,1194,"50%"]
+t1750 = [1750,1338,1331,1318,1306,1279,1254,1233,1218,"50%"]
+t752 = [752,1356,1349,1336,1323,1295,1269,1249,1236,"50%"]
+t254 = [254,1350,1342,-1,-1,1299,-1,-1,1254,"50%"]
+t756 = [756,1380,1373,1360,1348,1321,1296,1275,1260,"50%"]
+t762 = [762,1434,1427,1414,1402,1375,1350,1329,1314,"50%"]
+t334 = [334,1439,1433,-1,-1,1390,1365,1345,1332,"50%"]
+t596 = [596,-1,-1,-1,-1,1439,1415,1395,1380,"50%"]
 
 Mon = [t551,t1505,t1627,t629,t761,t763,t399,t541,t545,t752,t254,t762,t334,t596]
 Tue = [t551,t1505,t1627,t629,t255,t757,t259,t761,t763,t399,t541,t545,t1512,t246,t752,t254,t756,t762,t334,t596]
@@ -63,13 +63,14 @@ Sun = [t551,t1635,t637,t139,t502,t604,t1618,t620,t596]
 
 week = [Mon,Tue,Wed,Thu,Fri,Sat,Sun]
 
+counter = 0
+
 for x in range(0, len(week[weekday])):
 	if week[weekday][x][stationEnd]-week[weekday][x][stationStart] >0:
 		if week[weekday][x][stationStart]>rideTime:
 			if week[weekday][x][stationEnd]>0:
 				print '{:>4}'.format(str(week[weekday][x][0]))+" "+'{:>8}'.format(stations[(stationStart-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationStart]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationStart]%60))+")  -> "+'{:>8}'.format(stations[(stationEnd-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationEnd]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationEnd]%60))+") Discount: "+week[weekday][x][9]
-	
-	if week[weekday][x][stationEnd]-week[weekday][x][stationStart] <0:
-			if week[weekday][x][stationStart]>rideTime:
-				if week[weekday][x][stationEnd]>0:
-					print '{:>4}'.format(str(week[weekday][x][0]))+" "+'{:>8}'.format(stations[(stationStart-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationStart]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationStart]%60))+")  -> "+'{:>8}'.format(stations[(stationEnd-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationEnd]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationEnd]%60))+") Discount: "+week[weekday][x][9]
+				counter=counter+1
+
+if counter == 0:
+	print "No available train, try again."
