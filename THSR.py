e8999b9b29548431e3a1ac30755f6edc236f6655
Chris Lai <mlkh0225@gmail.com>
2015-02-26 13:33:33 +0800

    v0.2 完整五折車次（疏運除外）

--- THSR.py
+++ THSR.py
@@ -39,24 +39,37 @@ t399 = [399,1332,-1,1351,-1,1384,1408,1426,1439,"50%"]
 t541 = [541,1350,1358,1372,1383,1410,-1,-1,-1,"50%"]
 t545 = [545,1380,1388,1402,1413,1439,-1,-1,-1,"50%"]
 
-s_weekday1 = [t551,t1505,t1627,t629,t761,t763,t399,t541,t545]
-s_weekday2 = [t551,t1505,t1627,t629,t255,t757,t259,t761,t763,t399,t541,t545]
-s_weekday3 = [t551,t1505,t1627,t629,t763,t399,t541,t545]
-s_weekday4 = [t551,t1505,t763,t399,t541,t545]
-s_weekday5 = [t551,t1505]
-s_weekday6 = [t551,t1247,t749,t753,t255]
-s_weekday7 = [t551,t1635,t637,t139]
+t502 = [502,-1,-1,-1,390,417,429,443,450,"50%"]
+t604 = [604,396,411,431,457,484,496,509,516,"50%"]
+t1512 = [1512,-1,-1,-1,499,526,538,551,558,"50%"]
+t1618 = [1618,498,513,534,559,586,598,611,618,"50%"]
+t620 = [620,516,529,549,575,603,616,629,636,"50%"]
+t246 = [246,1194,-1,-1,1239,-1,-1,1282,1290,"50%"]
+t1750 = [1750,1218,1233,1254,1279,1306,1318,1331,1338,"50%"]
+t752 = [752,1236,1249,1269,1295,1323,1336,1349,1356,"50%"]
+t254 = [254,1254,-1,-1,1299,-1,-1,1342,1350,"50%"]
+t756 = [756,1260,1275,1296,1321,1348,1360,1373,1380,"50%"]
+t762 = [762,1314,1329,1350,1375,1402,1414,1427,1434,"50%"]
+t334 = [334,1332,1345,1365,1390,-1,-1,1433,1439,"50%"]
+t596 = [596,1380,1395,1415,1439,-1,-1,-1,-1,"50%"]
 
-s_days = [s_weekday1,s_weekday2,s_weekday3,s_weekday4,s_weekday5,s_weekday6,s_weekday7]
+Mon = [t551,t1505,t1627,t629,t761,t763,t399,t541,t545,t752,t254,t762,t334,t596]
+Tue = [t551,t1505,t1627,t629,t255,t757,t259,t761,t763,t399,t541,t545,t1512,t246,t752,t254,t756,t762,t334,t596]
+Wed = [t551,t1505,t1627,t629,t763,t399,t541,t545,t1512,t246,t752,t254,t756,t762,t334,t596]
+Thu = [t551,t1505,t763,t399,t541,t545,t1512,t752,t254,t762,t334,t596]
+Fri = [t551,t1505,t596]
+Sat = [t551,t1247,t749,t753,t255,t1750,t752,t596]
+Sun = [t551,t1635,t637,t139,t502,t604,t1618,t620,t596]
 
-train = [t551]
+week = [Mon,Tue,Wed,Thu,Fri,Sat,Sun]
 
-if stationEnd>stationStart:	
-	for x in range(0, len(s_days[weekday])):
-		if s_days[weekday][x][stationStart]>rideTime:
-			if s_days[weekday][x][stationEnd]>0:
-				print '{:>4}'.format(str(s_days[weekday][x][0]))+" "+'{:>8}'.format(stations[(stationStart-1)])+"("+'{:>2}'.format(str(s_days[weekday][x][stationStart]/60))+":"+'{:>2}'.format(str(s_days[weekday][x][stationStart]%60))+")  -> "+'{:>8}'.format(stations[(stationEnd-1)])+"("+'{:>2}'.format(str(s_days[weekday][x][stationEnd]/60))+":"+'{:>2}'.format(str(s_days[weekday][x][stationEnd]%60))+") Discount: "+s_days[weekday][x][9]
-
-
-#print str(s_weekday1[0][5]/60)+":"+str(s_weekday1[0][5]%60)
-
+for x in range(0, len(week[weekday])):
+	if week[weekday][x][stationEnd]-week[weekday][x][stationStart] >0:
+		if week[weekday][x][stationStart]>rideTime:
+			if week[weekday][x][stationEnd]>0:
+				print '{:>4}'.format(str(week[weekday][x][0]))+" "+'{:>8}'.format(stations[(stationStart-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationStart]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationStart]%60))+")  -> "+'{:>8}'.format(stations[(stationEnd-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationEnd]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationEnd]%60))+") Discount: "+week[weekday][x][9]
+	
+	if week[weekday][x][stationEnd]-week[weekday][x][stationStart] <0:
+			if week[weekday][x][stationStart]>rideTime:
+				if week[weekday][x][stationEnd]>0:
+					print '{:>4}'.format(str(week[weekday][x][0]))+" "+'{:>8}'.format(stations[(stationStart-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationStart]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationStart]%60))+")  -> "+'{:>8}'.format(stations[(stationEnd-1)])+"("+'{:>2}'.format(str(week[weekday][x][stationEnd]/60))+":"+'{:>2}'.format(str(week[weekday][x][stationEnd]%60))+") Discount: "+week[weekday][x][9]
